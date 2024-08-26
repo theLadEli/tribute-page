3f6462651559e1099f721fcb029038127eaf7324
Eli <109231330+theLadEli@users.noreply.github.com>
2024-08-26 19:47:23 +0100

    Started quotes section

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function closeMobileMenu() {
     document.getElementById("mobile_menu").addClass("hidden")
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
