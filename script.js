33669f70089e7160ce8a181f20c90ca2ed2a2add
Eli <109231330+theLadEli@users.noreply.github.com>
2024-08-27 15:54:24 +0100

    Mobile optimised Quotes section

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-function closeMobileMenu() {
-    document.getElementById("mobile_menu").addClass("hidden")
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
+    var elems = document.querySelectorAll('.carousel');
 
-  var instances = M.Carousel.init(elems, {
-      fullWidth: true,
-      indicators: true
-  });
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+    });
 
-  // Next and Previous Controls
-  document.querySelector('.next').addEventListener('click', function () {
-      instances[0].next();
-  });
+    // Next and Previous Controls
+    document.querySelector('.next').addEventListener('click', function () {
+        instances[0].next();
+    });
 
-  document.querySelector('.prev').addEventListener('click', function () {
-      instances[0].prev();
-  });
+    document.querySelector('.prev').addEventListener('click', function () {
+        instances[0].prev();
+    });
+
+
+    var sideNavElems = document.querySelectorAll('.sidenav');
+    var sideNavInstance = M.Sidenav.init(sideNavElems);
+
+    function openSideNav () {
+        sideNavInstance.open();
+    }
+
+
 });
